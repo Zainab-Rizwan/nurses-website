8292db627226920035761fae32891857f342199a
Zainab-Rizwan <zainabriz0027@gmail.com>
2023-03-18 19:02:36 +0500

    Update # 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#E3E8E9',
+      main: '#818E9A',
+      dark: '#1D2842',
+      contrastText: 'rgba(0, 0, 0, 0.87)',
+    },
+    custom2: {
+      main: '#81A3BF',
+      light: '#CCD4DF',
+      dark: '#1A667A'
+    },
+  },
+  typography: {
+    fontFamily: 'Raleway, Arial',
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
     <App />
   </React.StrictMode>
 );
