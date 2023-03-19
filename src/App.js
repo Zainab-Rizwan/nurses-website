8419e368237c84ed9fe18fe06b152f8114d55447
Zainab-Rizwan <zainabriz0027@gmail.com>
2023-03-19 21:50:43 +0500

    Strctured code around ant-design #1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import './App.css';
-import Sidenav from './Sidenav';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
-import Dashboard from './employer-pages/dashboard';
-import Account from './employer-pages/account';
-import Applicants from './employer-pages/applicants';
-import Disciplines from './employer-pages/disciplines';
-import Facilities from './employer-pages/facilities';
-import Jobs from './employer-pages/jobs';
+import {BrowserRouter} from "react-router-dom";
+import Router from './pages/router';
 
-
-function App() {
+const App = () => {
   return (
-    <>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" exact element={<Dashboard />}></Route>
-      <Route path="/account" exact element={<Account />}></Route>
-      <Route path="/applicants" exact element={<Applicants />}></Route>
-      <Route path="/disciplines" exact element={<Disciplines />}></Route>
-      <Route path="/facilities" exact element={<Facilities />}></Route>
-      <Route path="/jobs" exact element={<Jobs />}></Route>
-    </Routes>
+      <Router />
     </BrowserRouter>
-    </>
   );
-}
+};
 
 export default App;
--- src/employer-pages/applicants.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Applicants() {
-    return(
-        <div>Applicants</div>
-    )
-}
